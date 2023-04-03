2b6e9c31ec17064f86834d42d7c444bea9d58774
sport-Char <126862108+sport-Char@users.noreply.github.com>
2023-04-03 10:53:46 +0200

    Update padj_player.py

--- padj_player.py
+++ padj_player.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
     new_df.reset_index(drop=True,inplace=True)
     new_df["Posession"]= "Unknow"
     df["PDI"]=0
-    st.sidebar.title("Variable de sélection")
+    st.sidebar.title("Variable de sélection Offensive")
     test = []
     i= 0
     choice_variable_off = st.sidebar.multiselect("Variables",df.select_dtypes(["float","int"]).columns)
@@ -27,13 +27,22 @@ if uploaded_file is not None:
     if "Unknow" not in edited_df["Posession"].values and len(choice_variable_off)>0:
         df = df.merge(edited_df, on='Équipe dans la période sélectionnée', how = "left")
         df["Posession"]= df["Posession"].astype(float)
-        st.sidebar.title("Influence de chaque parametre")
+        st.sidebar.title("Influence de chaque parametre offensif")
         for variable in choice_variable_off:
             var = st.sidebar.slider(variable, 0, 100, 100)
             df[variable] = (df[variable] * 2 /(1 + np.exp(-0.1*(50-df["Posession"]))))
             df[variable] = stats.zscore(df[variable])
             #st.write(df[[variable,'Joueur']])
             df["PDI"] += df[variable]*(var/100)
+            
+        if len(choice_variable_def)>0:
+            st.sidebar.title("Influence de chaque parametre défensif")
+            for variable_def in choice_variable_def:
+                var = st.sidebar.slider(variable_def, 0, 100, 100)
+                df[variable_def] = (df[variable_def] * 2 /(1 + np.exp(-0.1*(df["Posession"]-50))))
+                df[variable_def] = stats.zscore(df[variable_def])
+            #st.write(df[[variable,'Joueur']])
+                df["PDI"] += df[variable_def]*(var/100)
 
         df = df.sort_values(by='PDI', ascending=False)
         df.reset_index(drop=True, inplace=True)
